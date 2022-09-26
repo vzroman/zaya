9af243303aa85c48c74987c175f6c9c2e6cb9688
roman <vzroman@gmail.com>
2022-09-26 18:25:33 +0600

    debug

--- src/zaya_copy.erl
+++ src/zaya_copy.erl
@@ -292,7 +292,7 @@ copy_request(#{
     batch = []
   },
 
-  {ok, Unlock} = elock:lock(?locks, Source, _IsShared = true, [node()], _Timeout = ?infinity  ),
+  {ok, Unlock} = elock:lock(?locks, Source, _IsShared = true, _Timeout = ?infinity ),
 
   try
       #s_acc{ batch = TailBatch, hash = TailHash } = TailState =
